076cc95fa75ab97a121238237c3e18ab96dddfc6
Christian Rodriguez <rodchris3090452@gmail.com>
2024-03-06 13:05:52 -0500

    completed filters with urls

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 import { Hero, SearchBar, CustomFilter, CarCard } from "@/components";
+import { fuels, yearsOfProduction } from "@/constants";
+import { HomeProps } from "@/types";
 import { fetchCars } from "@/utils";
-import Image from "next/image";
 
-export default async function Home() {
-
-  const allCars = await fetchCars();
+export default async function Home({ searchParams }: HomeProps) {
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || "",
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || "",
+    limit: searchParams.limit || 10,
+    model: searchParams.model || "",
+  });
 
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars
 
@@ -15,14 +21,15 @@ export default async function Home() {
         <div className="home__text-container">
           <h1 className="text-4xl font-extrabold">Car Catalog</h1>
           <p>Explore your next car!</p>
-          <div className="home__filters">
-            <SearchBar />
-            <div className="home__filter-container">
-              {/* <CustomFilter title='fuel' />
-              <CustomFilter title='year' /> */}
-            </div>
+        </div>
+        <div className="home__filters">
+          <SearchBar />
+          <div className="home__filter-container">
+            <CustomFilter title='fuel' options={fuels} />
+            <CustomFilter title='year' options={yearsOfProduction} />
           </div>
         </div>
+
         {!isDataEmpty ? (
           <section>
             <div className="home__cars-wrapper">
--- utils/index.ts
+++ utils/index.ts
@@ -1,19 +1,37 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
+    const { manufacturer, year, model, limit, fuel } = filters;
+
     const headers = {
         'X-RapidAPI-Key': 'ff672c76e1msh94d2eed5670ee6ep19a1b5jsnf43f17acb594',
         'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
     }
 
-    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3', {
-        headers: headers
-    });
+    const response = await fetch(
+        `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
+        {
+            headers: headers,
+        }
+    );
 
     const result = await response.json();
 
     return result
 }
+
+export const updateSearchParams = (type: string, value: string) => {
+    // Get the current URL search params
+    const searchParams = new URLSearchParams(window.location.search);
+  
+    // Set the specified search parameter to the given value
+    searchParams.set(type, value);
+  
+    // Set the specified search parameter to the given value
+    const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
+  
+    return newPathname;
+  };
 
 
 export const calculateCarRent = (city_mpg: number, year: number) => {
@@ -35,13 +53,13 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
 export const generateCarImageUrl = (car: CarProps, angle?: string) => {
     const url = new URL("https://cdn.imagin.studio/getimage");
     const { make, model, year } = car;
-  
+
     url.searchParams.append('customer', 'hrjavascript-mastery' || '');
     url.searchParams.append('make', make);
     url.searchParams.append('modelFamily', model.split(" ")[0]);
     url.searchParams.append('zoomType', 'fullscreen');
     url.searchParams.append('modelYear', `${year}`);
     url.searchParams.append('angle', `${angle}`);
-  
+
     return `${url}`;
 }
